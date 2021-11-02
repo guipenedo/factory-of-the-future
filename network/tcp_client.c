4826e4a156918446ff8ff88eace28e19d332ddfb
guipenedo <nostrumg@gmail.com>
2021-11-02 17:42:35 +0100

    threaded tcp and client server

--- network/tcp_client.c
+++ network/tcp_client.c
@@ -1,73 +1,134 @@
 // RSX101 CLIENT TCP
- #include <unistd.h>
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <string.h> 
-#include <sys/socket.h> 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <pthread.h>
 
 
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
 
-#define MAX 80 
-#define PORT 8080 
-#define SA struct sockaddr 
+#define MAX 80
+#define PORT 8080
+#define SA struct sockaddr
+
+typedef struct ServerData {
+    pthread_cond_t has_command;
+    pthread_mutex_t command_mutex;
+    int commandId;
+    int sockfd;
+    char response[MAX];
+} ServerData;
 
 
-void func(int sockfd) 
-{ 
-	char buff[MAX]; 
-	int n; 
-	for (;;) { 
-		bzero(buff, sizeof(buff)); 
-		printf("Enter the string : "); 
-		n = 0; 
-		while ((buff[n++] = getchar()) != '\n') 
-			; 
-		write(sockfd, buff, sizeof(buff)); 
-		bzero(buff, sizeof(buff)); 
-		read(sockfd, buff, sizeof(buff)); 
-		printf("From Server : %s", buff); 
-		if ((strncmp(buff, "exit", 4)) == 0) { 
-			printf("Client Exit...\n"); 
-			break; 
-		} 
-	} 
-} 
+void send_command_to_server(ServerData * data, int commandId, char * response) {
+    pthread_mutex_lock(&(data->command_mutex));
 
-int main() 
-{ 
-	int sockfd; 
-	struct sockaddr_in servaddr; 
+    data->commandId = commandId;
 
-	// socket create and varification 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-	if (sockfd == -1) { 
-		printf("socket creation failed...\n"); 
-		exit(0); 
-	} 
-	else
-		printf("Socket successfully created..\n"); 
-	bzero(&servaddr, sizeof(servaddr)); 
+    pthread_cond_signal(&(data->has_command));
 
-	// assign IP, PORT 
-	servaddr.sin_family = AF_INET; 
-	servaddr.sin_addr.s_addr = inet_addr("192.168.0.37"); 
-	servaddr.sin_port = htons(PORT); 
+    strcpy(response, data->response);
 
-	// connect the client socket to server socket 
-	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
-		printf("connection with the server failed...\n"); 
-		exit(0); 
-	} 
-	else
-		printf("connected to the server..\n"); 
+    pthread_mutex_unlock(&(data->command_mutex));
+}
 
-	// function for chat 
-	func(sockfd); 
 
-	// close the socket 
-	close(sockfd); 
+void * interact_with_server (void * p_data) {
+    ServerData * data = (ServerData *) p_data;
+
+    char buff[MAX];
+
+    while (1) {
+        pthread_mutex_lock(&(data->command_mutex));
+        pthread_cond_wait(&(data->has_command), &(data->command_mutex));
+
+        // if msg contains "Exit" then server exit and chat ended.
+        if (data->commandId == - 1) {
+            printf("Server Exit...\n");
+            break;
+        }
+
+        bzero(buff, MAX);
+        sprintf(buff, "%d", data->commandId);
+
+        write(data->sockfd, buff, sizeof(buff));
+
+        read(data->sockfd, data->response, sizeof(data->response));
+
+        pthread_mutex_unlock(&(data->command_mutex));
+    }
+}
+
+
+void connect_to_tcp_server(const char * server_addr, ServerData * data) {
+    struct sockaddr_in servaddr;
+    pthread_t interact_server_thread;
+    pthread_cond_init(&(data->has_command), NULL);
+    pthread_mutex_init(&(data->command_mutex), NULL);
+
+    // socket create and varification
+    data->sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (data->sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
+
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr(server_addr);
+    servaddr.sin_port = htons(PORT);
+
+    // connect the client socket to server socket
+    if (connect(data->sockfd, (SA*) &servaddr, sizeof(servaddr)) != 0) {
+        printf("connection with the server failed...\n");
+        exit(0);
+    }
+    else
+        printf("connected to the server..\n");
+
+    pthread_create(&interact_server_thread, NULL, interact_with_server, data);
+
+    return data;
+}
+
+int main() {
+    int sockfd;
+    struct sockaddr_in servaddr;
+
+    // socket create and varification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
+
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr("192.168.0.37");
+    servaddr.sin_port = htons(PORT);
+
+    // connect the client socket to server socket
+    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
+        printf("connection with the server failed...\n");
+        exit(0);
+    }
+    else
+        printf("connected to the server..\n");
+
+    // function for chat
+    func(sockfd);
+
+    // close the socket
+    close(sockfd);
 } 
 
--- network/tcp_server.c
+++ network/tcp_server.c
@@ -1,100 +1,134 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <netinet/in.h> 
-#include <stdlib.h> 
-#include <string.h> 
-#include <sys/socket.h> 
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
+#define MAX_CLIENTS 8
 
-#define MAX 80 
+#define MAX 80
 
-#define PORT 8080 
-#define SA struct sockaddr 
+#define PORT 8080
+#define SA struct sockaddr
 
-// Function designed for chat between client and server. 
-void func(int sockfd) 
-{ 
-	char buff[MAX]; 
-	int n; 
-	// infinite loop for chat 
-	for (;;) { 
-		bzero(buff, MAX); 
+#define GET_SENSOR_LIST 0
+#define GET_ACTUATOR_LIST 1
 
-		// read the message from client and copy it in buffer 
-		read(sockfd, buff, sizeof(buff)); 
-		// print buffer which contains the client contents 
-		printf("From client: %s\t To client : ", buff); 
-		bzero(buff, MAX); 
-		n = 0; 
-		// copy server message in the buffer 
-		while ((buff[n++] = getchar()) != '\n') 
-			; 
+typedef struct ClientThreadArg {
+    void (* func) (int, char *);
+    int connfd;
+} ClientThreadArg;
 
-		// and send that buffer to client 
-		write(sockfd, buff, sizeof(buff)); 
+void * serve_client(void * p_data){
+    ClientThreadArg * thread_arg = (ClientThreadArg *) p_data;
+    int connfd = thread_arg->connfd;
+    void (* func) (int, char *) = thread_arg->func;
+    free(thread_arg);
 
-		// if msg contains "Exit" then server exit and chat ended. 
-		if (strncmp("exit", buff, 4) == 0) { 
-			printf("Server Exit...\n"); 
-			break; 
-		} 
-	} 
-} 
+    char buff[MAX], res_buff[MAX];
+    // infinite loop for chat
+    while (1) {
+        bzero(buff, MAX);
+        bzero(res_buff, MAX);
+
+        // read the message from client and copy it in buffer
+        read(connfd, buff, sizeof(buff));
+
+        // actually handle whatever we have to do
+        int commandId;
+        int conv = sscanf(buff, "%d", &commandId);
+        if (conv > 0)
+            func(commandId, res_buff);
+
+        // if msg contains "Exit" then server exit and chat ended.
+        if (strncmp("exit", buff, 4) == 0) {
+            printf("Server Exit...\n");
+            break;
+        }
+
+        // print buffer which contains the client contents
+        printf("From client: %s\n To client : %s\n", buff, res_buff);
+
+        // and send that buffer to client
+        write(connfd, res_buff, sizeof(res_buff));
+    }
+
+    close(connfd);
+}
+
+int accept_tcp_connections(void (* func) (int, char *)) {
+    pthread_t client_t[MAX_CLIENTS];
+
+    int sockfd, client_i = 0;
+    socklen_t len;
+    struct sockaddr_in servaddr;
+    struct sockaddr_storage serverStorage;
+
+    // socket create and verification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    } else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
+
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET; // TCP
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_addr.s_addr = inet_addr("0.0.0.0");
+    servaddr.sin_port = htons(PORT);
+
+    // Binding newly created socket to given IP and verification
+    if ((bind(sockfd, (SA *) &servaddr, sizeof(servaddr))) != 0) {
+        printf("socket bind failed...\n");
+        exit(0);
+    } else
+        printf("Socket successfully binded..\n");
+
+    // Now server is ready to listen and verification
+    if ((listen(sockfd, 5)) != 0) {
+        printf("Listen failed...\n");
+        exit(0);
+    } else
+        printf("Server listening..\n");
+
+    while (1) {
+        ClientThreadArg * args = malloc (sizeof (ClientThreadArg));
+        args->func = func;
+        //Accept call creates a new socket for the incoming connection
+        len = sizeof serverStorage;
+        args->connfd = accept(sockfd, (SA *) &serverStorage, &len);
+        if (args->connfd < 0) {
+            printf("server acccept failed...\n");
+            exit(0);
+        } else
+            printf("server acccept the client...\n");
+
+        //for each client request creates a thread and assign the client request to it to process
+        //so the main thread can entertain next request
+        if(pthread_create(&client_t[client_i++], NULL, serve_client, args) != 0) {
+            printf("Failed to create thread\n");
+            free(args);
+        }
+    }
+}
+
+void test_commands (int commandId, char * response){
+    if (commandId == GET_SENSOR_LIST){
+        printf("SENSOR LIST IS HERE\n");
+        strncpy(response, "SENSOR LIST IS HERE", 20);
+    } else if (commandId == GET_ACTUATOR_LIST){
+        printf("ACTUATOR LIST IS HERE\n");
+        strncpy(response, "ACTUATOR LIST IS HERE", 22);
+    }
+}
 
 // Driver function 
-int main() 
-{ 
-	int sockfd, connfd; 
-	socklen_t len;
-	struct sockaddr_in servaddr, cli; 
-
-	// socket create and verification 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-	if (sockfd == -1) { 
-		printf("socket creation failed...\n"); 
-		exit(0); 
-	} 
-	else
-		printf("Socket successfully created..\n"); 
-	bzero(&servaddr, sizeof(servaddr)); 
-
-	// assign IP, PORT 
-	servaddr.sin_family = AF_INET; // TCP
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-        servaddr.sin_addr.s_addr = inet_addr("192.168.0.37");
-	servaddr.sin_port = htons(PORT); 
-
-	// Binding newly created socket to given IP and verification 
-	if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
-		printf("socket bind failed...\n"); 
-		exit(0); 
-	} 
-	else
-		printf("Socket successfully binded..\n"); 
-
-	// Now server is ready to listen and verification 
-	if ((listen(sockfd, 5)) != 0) { 
-		printf("Listen failed...\n"); 
-		exit(0); 
-	} 
-	else
-		printf("Server listening..\n"); 
-	len = sizeof(cli); 
-
-	// Accept the data packet from client and verification 
-	connfd = accept(sockfd, (SA*)&cli, &len); 
-	if (connfd < 0) { 
-		printf("server acccept failed...\n"); 
-		exit(0); 
-	} 
-	else
-		printf("server acccept the client...\n"); 
-
-	// Function for chatting between client and server 
-	func(connfd); 
-
-	// After chatting close the socket 
-	close(sockfd); 
+int main() {
+    accept_tcp_connections(test_commands);
 } 
 
