1e7ffa82fdd8179ef696285fb96a619ffcc0e5dd
guipenedo <nostrumg@gmail.com>
2021-11-08 11:18:30 +0100

    added some sensor broadcast testing code

--- factory.c
+++ factory.c
@@ -1,11 +1,13 @@
 #include "network/tcp_ip.h"
 #include "interfaces/network_commands.h"
+#include "interfaces/peripherals_network.h"
 #include "utils/host_list.h"
 
 host_node * host_list;
 pthread_t server_thread;
 
 int fact_ID = -1;
+char cmd_args[MAX_ARGS_BUFFER_SIZE];
 
 ClientThreadData * connect_new_factory(char * args) {
     char ip_address[20];
@@ -19,20 +21,49 @@ ClientThreadData * connect_new_factory(char * args) {
     return newFactoryClient;
 }
 
+void trigger_alarm(int fact_id_alarm) {
+    // TODO
+}
+
 void handle_command(int commandId, char * args, char * response) {
     if (commandId == CMD_ANNOUNCE_NEW_FACTORY) {
         ClientThreadData * newFactoryClient = connect_new_factory(args);
 
         // tell new factory to also connect to our server
-        char send_args[MAX_BUFFER_SIZE];
         char ip_address[20];
         get_ip_address(ip_address);
-        sprintf(send_args, "%s %d", ip_address, fact_ID);
+        sprintf(cmd_args, "%s %d", ip_address, fact_ID);
         // send CMD_CONNECT_BACK_FACTORY
-        send_command_to_server(CMD_CONNECT_BACK_FACTORY, send_args, NULL, newFactoryClient);
+        send_command_to_server(CMD_CONNECT_BACK_FACTORY, cmd_args, NULL, newFactoryClient);
     } else if (commandId == CMD_CONNECT_BACK_FACTORY) {
         connect_new_factory(args);
+    } else if (commandId == CMD_SEND_SENSOR_DATA) {
+        int factId;
+        double temperature, humidity, pressure;
+        sscanf(args, "%d %lf %lf %lf", &factId, &temperature, &humidity, &pressure);
+        printf("Sensor data from fact_ID=%d: T=%lf H=%lf P=%lf\n", factId, temperature, humidity, pressure);
+    } else if (commandId == CMD_TRIGGER_ALARM) {
+        int fact_id_alarm;
+        sscanf(args, "%d", &fact_id_alarm);
+        printf("[!] Alarm triggered because of factory ID %d\n.", fact_id_alarm);
+        trigger_alarm(fact_id_alarm);
     }
+}
+
+void broadcast_sensor_data(SensorData sensorData) {
+    sprintf(cmd_args, "%d %lf %lf %lf", fact_ID, sensorData.temperature, sensorData.humidity, sensorData.pressure);
+    host_node * host = host_list;
+    while (host->next != NULL){
+        host = host->next;
+        send_command_to_server(CMD_SEND_SENSOR_DATA, cmd_args, NULL, host->host);
+    }
+}
+
+// TODO: replace with production version
+void read_sensor_data(SensorData * data) {
+    data->pressure = 1.34;
+    data->temperature = 20.4;
+    data->humidity = 99;
 }
 
 int main(int argc, char **argv) {
@@ -63,7 +94,9 @@ int main(int argc, char **argv) {
     printf("Successfully connected to the dashboard. This factory now has ID=%d\n", fact_ID);
 
     while(1) {
-        //TODO send sensor data to every host
+        SensorData sensorData;
+        read_sensor_data(&sensorData);
+        broadcast_sensor_data(sensorData);
         sleep(5);
     }
 
